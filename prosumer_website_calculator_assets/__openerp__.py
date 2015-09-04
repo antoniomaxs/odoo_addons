b83345429ab27db7da69e402f2e3dd110ef9efb7
antonio <antonio.maximo@fagar.com>
2015-09-04 16:00:21 +0200

    Assets calculadoras prosumer

---
+++ prosumer_website_calculator_assets/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#################################################################
+# antonio.gonzalez                                  #
+#################################################################
+
+{
+    'name' : 'Calculator Website Assets',
+    'version': '1.0',
+    'depends': [],
+    'author': 'Ingeniería Cloud',
+    'website':'http://ingenieriacloud.com',
+    'category': 'Calculadoras Web',
+    'description': """
+Añade los Assets(JS y CSS) necesarios para el funcionamiento de las calculadoras de PROSUMER en el WEBSITE    """,
+    'website': '',
+    'data': [
+        'views/website_calculator_assets.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+}
