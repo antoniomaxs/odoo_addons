0a8422c3c3ef4d295b4c4045339231f4833ae9ab
antonio <antonio.maximo@fagar.com>
2015-09-04 15:55:04 +0200

    Modulos recibos inmobiliaria

---
+++ recibos/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#################################################################
+# antonio.gonzalez									#
+#################################################################
+
+import recibos
+import product
+import analytic
+import crm_lead
