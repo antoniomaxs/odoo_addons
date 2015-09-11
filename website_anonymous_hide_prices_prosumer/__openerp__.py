9ed6c46d4ec534d19751d475c414f67245aa96db
antonio <antonio.maximo@fagar.com>
2015-09-11 21:27:49 +0200

    Error detectado en prosumer con el módulo. https://github.com/scaldewi/website_anonymous_hide_prices/blob/master/templates.xml
    Se ha realizado una adaptación para que funcione correctamente.

--- website_anonymous_hide_prices_prosumer/__openerp__.py
+++ website_anonymous_hide_prices_prosumer/__openerp__.py
@@ -6,11 +6,11 @@
         Hide prices, quantity select and "add to cart" button if user is not authenticated""",
 
     'description': """
-        Adds some Qweb views to replace sections on page which will be hidden if user ist not authenticated
+        Adds some Qweb views to replace sections on page which will be hidden if user ist not authenticated. Fork of https://github.com/scaldewi/website_anonymous_hide_prices
     """,
 
-    'author': "Michael Hucke",
-    'website': "http://www.hucke-media.com",
+    'author': 'Ingeniería Cloud',
+    'website':'http://ingenieriacloud.com',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
